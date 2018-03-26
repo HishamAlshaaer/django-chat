043b2856389680e0cd1e73fbc9bd95b807902529
Jevin Subedi <jevinsubedi@gmail.com>
2018-03-26 00:42:27 -0500

    Created a Chat Room Model Class, registered to admin, and rendered it to index.html

--- multichat/chat/models.py
+++ multichat/chat/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from six import python_2_unicode_compatible
 
 # Create your models here.
+
+@python_2_unicode_compatible
+class Room(models.Model):
+    '''
+    A room for people to chat in.
+    '''
+
+    # Room title
+    title = models.CharField(max_length=255)
+
+    # If only 'staff' users are allowed (is_staff on djano's User)
+    staff_only = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
