48b144d854f821e36d47ff8d8196fb119b048ab9
Jevin Subedi <jevinsubedi@gmail.com>
2018-04-03 14:02:25 -0500

    Included websocket_routing and custom_routing.

--- multichat/chat/routing.py
+++ multichat/chat/routing.py
@@ -1,4 +1,4 @@
-from channels import route
+from channels import route, include
 from .consumers import ws_connect, ws_receive, ws_disconnect, chat_join, chat_leave, chat_send
 
 # There's no path matching on these routes; we just rely on the matching
@@ -23,4 +23,22 @@ custom_routing = [
         route("chat.receive", chat_join, command="^join$"),
         route("chat.receive", chat_leave, command="^leave$"),
         route("chat.receive", chat_send, command="^send$"),
+]
+
+# The channel routing defines what channels get handled by what consumers,
+# including optional matching on message attributes. In this example, we match
+# on a path prefix, and then include routing from the chat module.
+channel_routing = [
+    # Include sub-routing from an app.
+    include('chat.routing.websocket_routing', path=r"^/chat/stream"),
+
+    # Custom handler for message sending (see Room.send_message).
+    # Can't go in  the include above as it's not a got a 'path' attribute to match on.
+
+    include("chat.routing.custom_routing"),
+
+    # A default "http.request" route is always inserted by Django at the end of the routing_list
+    # that routes all unmatched HTTP requests to the Django view system. If you want lower-level
+    # HTTP handling -e.g. long-polling - you can do it here and route by path, and let the rest
+    # fall through to normal views.
 ]
