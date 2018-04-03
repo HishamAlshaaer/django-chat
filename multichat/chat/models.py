0d9e0db5f19da1596ad3f9fd2b39c596d2ed0c0a
Jevin Subedi <jevinsubedi@gmail.com>
2018-04-03 13:09:00 -0500

    Added a function to send a message to the room on behalf of a user.

--- multichat/chat/models.py
+++ multichat/chat/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from six import python_2_unicode_compatible
 from channels import Group
+import json
+
+from .settings import MSG_TYPE_MESSAGE
 
 # Create your models here.
 
@@ -26,3 +29,15 @@ class Room(models.Model):
         messages as they are generated.
         '''
         return Group('room-%s' % self.id)
+
+    def send_message(self, message, user, msg_type=MSG_TYPE_MESSAGE):
+        """
+        Called to send a message to the the room on behalf of a user.
+        """
+
+        final_msg = {'room': str(self.id), 'message': message, 'username': user.username, 'msg_type': msg_type}
+
+        # Send out the message to everyone in the room
+        self.websocket_group.send(
+            {'text': json.dumps(final_msg)}
+        )
