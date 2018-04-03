985664097aedf13f8f9b66ce2a357209a714ea75
Jevin Subedi <jevinsubedi@gmail.com>
2018-04-03 14:57:48 -0500

    Added Some Helpful Debugging Messages

--- multichat/static/main.js
+++ multichat/static/main.js
@@ -3,5 +3,16 @@ $(function () {
     // Correctly decide between ws:// and wss://
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     var ws_path = ws_scheme + '://' + window.location.host + "/chat/stream/";
+    console.log("Connecting to "+ ws_path);
     var socket = new ReconnectingWebSocket(ws_path);
+
+
+    // Helpful Debugging
+    socket.onopen = function () {
+        console.log("Connected to chat socket");
+    };
+    socket.onclose = function () {
+        console.log("Disconnected From chat socket");
+    }
+
 });
