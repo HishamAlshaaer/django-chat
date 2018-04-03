9cbead7ffcb93e1277e813c41710e2b7ed240297
Jevin Subedi <jevinsubedi@gmail.com>
2018-04-03 14:53:17 -0500

    Let's connect to our socket on the front end.

---
+++ multichat/static/main.js
@@ -0,0 +1,7 @@
+// Let's Connect to our socket:
+$(function () {
+    // Correctly decide between ws:// and wss://
+    var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+    var ws_path = ws_scheme + '://' + window.location.host + "/chat/stream/";
+    var socket = new ReconnectingWebSocket(ws_path);
+});
