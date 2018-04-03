7d018a8831e60c09dc3e4522a8083c91db4ee6e3
Jevin Subedi <jevinsubedi@gmail.com>
2018-04-03 15:31:11 -0500

    Connecting And Disconnecting From The Rooms

--- multichat/static/main.js
+++ multichat/static/main.js
@@ -13,6 +13,34 @@ $(function () {
     };
     socket.onclose = function () {
         console.log("Disconnected From chat socket");
-    }
+    };
+
+    // Says if we joined a room or not by if there's a div for it.
+    inRoom = function (roomId) {
+        return $("#room-" + room.Id).length > 0;
+    };
+
+    // Room join/leave
+    $('li.room-link').click(function () {
+        roomId = $(this).attr("data-room-id");
+
+        if (inRoom(roomId)) {
+            // Leave Room
+            $(this).removeClass("joined");
+            socket.send(JSON.stringify({
+                "command": "leave",     // determines which handler will be used (see chat/routing.py)
+                "room": roomId
+            }));
+
+        } else {
+            // Join room
+            $(this).addClass("joined");
+            socket.send(JSON.stringify({
+                "command": "join",
+                "room": roomId
+            }));
+        }
+
+    })
 
 });
