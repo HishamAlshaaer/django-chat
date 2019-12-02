f6438e64ad82c1ef77845119d1a1dcc2b73fcfc6
HishamAlshaaer <hisham.a.alshaaer@gmail.com>
2019-12-02 11:07:36 +0530

    changes made in consumers.py

--- multichat/chat/consumers.py
+++ multichat/chat/consumers.py
@@ -8,32 +8,6 @@ from .models import Room
 import json
 from channels import Channel
 
-'''
-Event Handler: When User Connects to the WebSocket
-When a user connects to the websocket, first of all we have to identify it. In this case,
-there is a decorator "channel_session_user_from_http" in the "django-channels" arsenal,
-but how does this work?
-This decorator takes a user from http session and inserts it into the "channel-base session"
-(which uses the django standard "SESSION_ENGINE"), after which it will be available in the
-"message.user" attribute.
-We must also take care of creating a "channel_session rooms" for the user. It is
-necessary to be able to subscribe the user to the events in a particular room.
-To do this, we create a chat/consumers.py file and declare ws_connect function in it:
-'''
-
-'''
-Event Handler: When User Disconnects from the WebSocket
-When a user disconnects from the websocket, we need to clean up his opening session.
-To identify the user, we'll need the "channel_session_user" which adds the user instance
-to the received message, based on the user ID from the "channel_session".
-Unlike "channel_session_user_from_http", it turns on channel session implicitly.
-
-In order to provide a possibility to send a message to all users who are in a particular
-room, "django-channels" provides an excellent opportunity to group "channels"
-
-Let's add a method "websocket_group" in "Room" models, which will return a unique
-"channels.Group" for each room through id:
-'''
 
 # This decorator copies the user from the HTTP session(only available in
 # websocket.connect or http.request.messages) to the channel session (available
